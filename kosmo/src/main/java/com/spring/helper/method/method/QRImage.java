60dcb50405d421bf09e1b51bbd08cf5652fe1937
kosmo43team3 <kosmo43team3@gmail.com>
2019-02-27 13:02:38 +0900

    qr코드에서 url 얻기 util 컨트롤러 -> method클래스 호출
    테스트는 부동산 페이지에 버튼

--- kosmo/src/main/java/com/spring/helper/method/method/QRImage.java
+++ kosmo/src/main/java/com/spring/helper/method/method/QRImage.java
@@ -4,6 +4,11 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import org.springframework.stereotype.Component;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
 
 @Component
 public class QRImage {
@@ -11,11 +16,20 @@ public class QRImage {
 	// 이미지 경로 넘겨 받기
 	public void cropImage(String filePath) throws Exception{
 		BufferedImage originalImgage = ImageIO.read(new File(filePath));
-		//x115 y115 부터 370x370픽셀 잘라내기
-		BufferedImage SubImgage = originalImgage.getSubimage(115, 115, 370, 370);
+		//x110 y110 부터 380x380픽셀 잘라내기
+		BufferedImage SubImgage = originalImgage.getSubimage(110, 110, 380, 380);
 		//잘라내고 난 이미지 경로
 		File outputfile = new File("C:\\Users\\panga\\Desktop\\croppedQr.jpg");
 		//위의 경로에 잘라낸 이미지 적용
 		ImageIO.write(SubImgage, "jpg", outputfile);
+	}
+
+	//qr에서 경로 얻기
+	public String readQRCode(String filePath) throws Exception {
+		//이미지 경로 받기
+		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new File(filePath)))));
+		//qr 경로 리턴
+		Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap);
+		return qrCodeResult.getText();
 	}
 }
